53f4a8fe850296c5ceb5b1dc195b50881cf3360a
SAMRA PET SHOP <ayasien1983@gmail.com>
2025-10-08 22:04:15 +0300

    new4433

---
+++ js/site.js
@@ -0,0 +1,10 @@
+﻿function downloadFileFromBlazor(filename, base64) {
+    const link = document.createElement('a');
+    link.href = 'data:text/csv;base64,' + base64;
+    link.download = filename;
+    link.click();
+}
+// Disable right-click on entire website
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault(); // Prevents right-click menu
+});
